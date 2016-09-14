8139af8bd5ca84587dab1746f9d2d5b0bb1dc2b1
77Vincent <wentianqi77@outlook.com>
2016-09-14 13:37:39 +0800

    color modify

--- templates/js/custom.js
+++ templates/js/custom.js
@@ -3,9 +3,9 @@
 	var i = Math.random();
 
 	selector.onmousemove = function() {
-		var a = parseInt(((event.clientY + event.clientX * 10 + 2000) / (selector.clientHeight + selector.clientWidth * 10 + 2000)) * 230);
-		var b = parseInt(((event.clientY + event.clientX * 5 + 5500) / (selector.clientHeight + selector.clientWidth * 5 + 4500)) * 220);
-		var c = parseInt(((event.clientY + event.clientX * 5 + 8000) / (selector.clientHeight + selector.clientWidth * 5 + 8000)) * 240);
+		var a = parseInt(((event.clientY + event.clientX * 10 + 2000) / (selector.clientHeight + selector.clientWidth * 10 + 1000)) * 230);
+		var b = parseInt(((event.clientY + event.clientX * 5 + 5000) / (selector.clientHeight + selector.clientWidth * 5 + 4000)) * 230);
+		var c = parseInt(((event.clientY + event.clientX * 5 + 7000) / (selector.clientHeight + selector.clientWidth * 5 + 7000)) * 240);
 
 		selector.style.backgroundColor = 'rgb(' + a + ',' + b + ',' + c + ')';
 	}
