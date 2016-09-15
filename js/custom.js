ecd676704988734383a86fe5968289b0f81a7edb
77Vincent <wentianqi77@outlook.com>
2016-09-15 14:19:51 +0800

    publish

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,12 @@
- 
+(function() {
+	var selector = document.querySelector('#sidebar');
+	var i = Math.random();
+
+	selector.onmousemove = function() {
+		var a = parseInt(((event.clientY + event.clientX * 10 + 2000) / (selector.clientHeight + selector.clientWidth * 10 + 1000)) * 230);
+		var b = parseInt(((event.clientY + event.clientX * 5 + 5000) / (selector.clientHeight + selector.clientWidth * 5 + 4000)) * 230);
+		var c = parseInt(((event.clientY + event.clientX * 5 + 7000) / (selector.clientHeight + selector.clientWidth * 5 + 7000)) * 240);
+
+		selector.style.backgroundColor = 'rgb(' + a + ',' + b + ',' + c + ')';
+	}
+})();
