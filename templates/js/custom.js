f59140975085dd1976f6aca8a4441a8e62c264d9
77Vincent <wentianqi77@outlook.com>
2016-09-15 22:47:18 +0800

    responsive, about page

--- templates/js/custom.js
+++ templates/js/custom.js
@@ -1,12 +1,40 @@
-(function() {
-	var selector = document.querySelector('#sidebar');
-	var i = Math.random();
+const Vinrainbow = function(selector) {
+	this.selector = selector;
+	this.factor = [];
+	this.por = this.selector.clientHeight / this.selector.clientWidth;
+	this.frame = this.selector.clientHeight + this.selector.clientWidth;
+	this.init();
+};
 
-	selector.onmousemove = function() {
-		var a = parseInt(((event.clientY + event.clientX * 10 + 2000) / (selector.clientHeight + selector.clientWidth * 10 + 1000)) * 230);
-		var b = parseInt(((event.clientY + event.clientX * 5 + 5000) / (selector.clientHeight + selector.clientWidth * 5 + 4000)) * 230);
-		var c = parseInt(((event.clientY + event.clientX * 5 + 7000) / (selector.clientHeight + selector.clientWidth * 5 + 7000)) * 240);
+Vinrainbow.prototype = {
+	init: function() {
+		this.calculate();
+	},
 
-		selector.style.backgroundColor = 'rgb(' + a + ',' + b + ',' + c + ')';
-	}
-})();
+	calculate: function() {
+		var self = this;
+
+		function getRandomArbitrary(min, max) {
+		   return Math.random() * (max - min) + min;
+		}
+
+		for (var i=0; i<6; i++) {
+			var e = Math.floor(getRandomArbitrary(800,2000));
+			this.factor.push(e);
+		}
+
+		this.selector.onmousemove = function() {
+			var move = event.clientY + event.clientX * 10;
+			/*	ALGORITHM
+			*/
+			var a = parseInt(((move + self.factor[0]) / (self.frame + self.factor[1])) * 60);
+			var b = parseInt(((move + self.factor[2]) / (self.frame + self.factor[3])) * 60);
+			var c = parseInt(((move + self.factor[4]) / (self.frame + self.factor[5])) * 60);
+
+			self.selector.style.backgroundColor = 'rgb(' + a + ',' + b + ',' + c + ')';
+		}
+	},
+};
+
+var rainbow = document.querySelector('#sidebar');
+new Vinrainbow(rainbow);
