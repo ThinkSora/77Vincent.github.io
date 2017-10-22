529703057808b371a76e467ee7b45cff5d83451f
77Vincent <wentianqi77@outlook.com>
2017-10-22 13:35:43 +0800

    add local eslint

--- src/app.js
+++ src/app.js
@@ -1,38 +1,39 @@
-import Vue from 'vue'
-import VueResource from 'vue-resource'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueResource from "vue-resource";
+import VueRouter from "vue-router";
 
 // Components
-import App from './components/App.vue'
-import About from './components/about.vue'
-import Blogs from './components/blogs.vue'
-import Post from './components/post.vue'
+import App from "./components/App.vue";
+import About from "./components/about.vue";
+import Blogs from "./components/blogs.vue";
+import Post from "./components/post.vue";
 
-Vue.use(VueResource)
-Vue.use(VueRouter)
+Vue.use(VueResource);
+Vue.use(VueRouter);
+console.log(111111111);
 
 const router = new VueRouter({
-    routes: [
-        {
-            path: '/',
-            component: Blogs
-        },
-        {
-            path: '/about',
-            component: About
-        },
-        {
-            path: '/blogs',
-            component: Blogs
-        },
-        {
-            path: '/post/:id',
-            component: Post
-        }]
+  routes: [
+    {
+      path: "/",
+      component: Blogs
+    },
+    {
+      path: "/about",
+      component: About
+    },
+    {
+      path: "/blogs",
+      component: Blogs
+    },
+    {
+      path: "/post/:id",
+      component: Post
+    }]
 });
 
 new Vue({
-    el: '#app',
-    router,
-    render: h => h(App)
+  el: "#app",
+  router,
+  render: h => h(App)
 });
