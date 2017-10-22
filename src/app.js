e7e86d54f518acd24cf89c281833ae9bb8909c86
77Vincent <wentianqi77@outlook.com>
2017-10-22 15:39:17 +0800

    add loading icon

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ import Post from "./components/post.vue";
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
-console.log(222222222);
 
 const router = new VueRouter({
   routes: [
--- src/components/blogs.vue
+++ src/components/blogs.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id='blogs'>
-    <section v-if="!item.fork && excludedItems.indexOf(item.name) < 0" v-for='item in blogs'>
+  <div id="blogs">
+    <div class="icon-loading" v-show="loading"></div>
+
+    <section v-if="!item.fork && excludedItems.indexOf(item.name) < 0" v-for="item in blogs">
       <h2>
-        <a v-bind:href='item.html_url'>{{item.name}}</a>
+        <a v-bind:href="item.html_url">{{item.name}}</a>
       </h2>
       <p>{{item.description}}</p>
     </section>
@@ -13,34 +15,39 @@
 export default {
   data() {
     return {
+      loading: true,
       blogs: '',
       excludedItems: [
         "angular-app",
         "instant"
       ]
-    }
-  }
-  ,
+    };
+  },
   methods: {
     getData(url) {
-      this.$http .get(url) .then(response=> {
-        this.$set(this._data, 'blogs', response.body)
-      }
-      , err=> {
-        console.log(err)
-      }
-      )
+      this.$http.get(url).then(response => {
+        this.$set(this.$data, 'blogs', response.body);
+        this.$data.loading = false;
+      }, err => {
+        console.log(err);
+      });
     }
-  }
-  ,
+  },
   mounted() {
-    this.getData("https://api.github.com/users/77Vincent/repos")
+    this.getData("https://api.github.com/users/77Vincent/repos");
   }
 }
 
 </script><style lang="scss">@import "../assets/scss/meta";
 #blogs {
   text-align: center;
+
+  section {
+    &:hover {
+      @include transition(background-color, 1.5s);
+      background-color: red;
+    }
+  }
 }
 
 </style>
