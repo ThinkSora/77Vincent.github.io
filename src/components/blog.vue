4d7f0226bc499a0c310195615e51794ff0897f74
77Vincent <wentianqi77@outlook.com>
2017-10-30 23:31:25 +0800

    add background, blogs info

---
+++ src/components/blog.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="blog">
+    <div class="icon-loading" v-show="loading"></div>
+    <vue-markdown :source="blog"></vue-markdown>
+  </div>
+</template>
+
+<script>
+import VueMarkdown from "vue-markdown";
+
+export default {
+  components: {
+    VueMarkdown
+  },
+  data() {
+    return {
+      loading: true,
+      blog: ""
+    };
+  },
+  mounted() {
+
+  }
+}
+
+</script>
+
+<style lang="scss">
+@import "../assets/scss/meta";
+
+#blogs {
+  text-align: center;
+}
+
+</style>
